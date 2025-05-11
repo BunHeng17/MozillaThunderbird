7937a8167a76305c6412de27a97e99fe218af38d
BunHeng17 <157872937+BunHeng17@users.noreply.github.com>
2025-05-11 10:18:07 +0700

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,12 @@
+const accordions = document.querySelectorAll('.accordion');
+    accordions.forEach((btn) => {
+      btn.addEventListener('click', () => {
+        btn.classList.toggle('active');
+        const panel = btn.nextElementSibling;
+        if (panel.style.display === 'block') {
+          panel.style.display = 'none';
+        } else {
+          panel.style.display = 'block';
+        }
+      });
+    });
